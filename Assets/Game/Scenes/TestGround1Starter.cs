d865911033c8702993fc938b02850ae50b841808
Ivan <71326217+RangerV107@users.noreply.github.com>
2020-10-06 17:42:57 +0300

    Folders restructure

--- Assets/Game/Scenes/TestGround1Starter.cs
+++ Assets/Game/Scenes/TestGround1Starter.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SamplesSceneStarter : Starter
+public class TestGround1Starter : Starter
 {
     public override void StarterSetup()
     {
---
+++ Assets/Game/Scripts/Processings/CameraFollowProc.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RangerV;
+
+public class CameraFollowProc : ProcessingBase, ICustomUpdate
+{
+    Group CamTargetGroup = Group.Create(new ComponentsList<CameraFollowCmp>());
+
+    public void CustomUpdate()
+    {
+        foreach (int entity in CamTargetGroup)
+        {
+            //Storage.GetComponent<>(entity)
+        }
+    }
+}

1681ce13d09fbf4765c0971cb6b4ea307bfb495e
Red <69144172+RedForest347@github.com>
2020-10-09 21:01:21 +0300

    A* Start

---
+++ Assets/Game/To tests/BaseAI/move.cs
@@ -0,0 +1,33 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.AI;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class move : MonoBehaviour
+{
+    public Transform target;
+    AIPath aiPath;
+    Seeker seeker;
+    //NavMeshAgent agent;
+
+
+    void Start()
+    {
+        NavMeshVisualizationSettings.showNavigation = 2;
+        aiPath = GetComponent<AIPath>();
+        seeker = GetComponent<Seeker>();
+
+    }
+
+    void Update()
+    {
+        //aiPath.;
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("путь начат");
+            seeker.StartPath(transform.position, target.position);
+        }
+    }
+}

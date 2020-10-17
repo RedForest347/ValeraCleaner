f1768cebb498e49d872b03d1eb8a78b0893b0c54
Red <69144172+RedForest347@github.com>
2020-10-17 13:16:50 +0300

    Revert "Merge pull request #4 from RedForest347/AIBase"
    This reverts commit 391f0519e9e38467a4fa0cc67ea19615c15ffd9f, reversing
    changes made to bf8e71acbeb65becbfc4aef6cef59802e18ee54a.

---
+++ Assets/Game/Scripts/Components/Physics2DCmp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using RangerV;
+
+[Component("Engine/Physics component 2D", "Rigidbody Icon")]
+public class Physics2DCmp : ComponentBase
+{
+    public Collider2D Collider;
+    public Rigidbody2D Rigidbody;
+
+    public Physics2DCmp()
+    {
+        Collider = null;
+        Rigidbody = null;
+    }
+
+    public Physics2DCmp(GameObject GO)
+    {
+        Collider = GO.GetComponent<Collider2D>();
+        Rigidbody = GO.GetComponent<Rigidbody2D>();
+    }
+}
---
+++ Assets/Game/Scripts/Processings/GunProc.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RangerV;
+
+public class GunProc : ProcessingBase, ICustomFixedUpdate
+{
+    Group GunGroup = Group.Create(new ComponentsList<GunCmp>());
+
+    public void CustomFixedUpdate()
+    {
+        foreach (int entity in GunGroup)
+        {
+            Shooting(entity);
+        }
+    }
+
+    void Shooting(int entity)
+    {
+        GunCmp gunCmp = Storage.GetComponent<GunCmp>(entity);
+
+        if (gunCmp.shooting && gunCmp.timer >= gunCmp.rateOfFire)
+        {
+            int entity_bullet = GameObject.Instantiate(gunCmp.Bullet, gunCmp.transform.position, gunCmp.transform.rotation).GetComponent<Entity>().entity;
+            Storage.GetComponent<Collision2DCmp>(entity_bullet).IgnoreColliders.AddRange(Storage.GetComponent<GunCmp>(entity).IgnoreColliders);
+            gunCmp.timer = 0;
+        }
+
+        gunCmp.timer++;
+        if(gunCmp.timer > 10000000)
+            gunCmp.timer = gunCmp.rateOfFire;
+    }
+
+    
+}

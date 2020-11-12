fa19071e7384f68a0c2ec10be182d4e0a6f8b4dd
Red <69144172+RedForest347@github.com>
2020-11-12 16:56:26 +0300

    Done PreparateDamage
    -он нужен для унификации нанесения урона врагам по сделанному рейкасту (с помощью сигнала PreparateDamageSignal)
    -он, в свою очередь, посылает сигнал для каждой отдельной сусности о нанесении урона ей
    -поправлено отображение зоны в HandlesExpansion

--- Assets/Game/DamageSystem/Processings/TakeDamageProc.cs
+++ Assets/Game/DamageSystem/Processings/TakeDamageProc.cs
@@ -3,36 +3,94 @@ using System.Collections.Generic;
 using UnityEngine;
 using RangerV;
 
-public class TakeDamageProc : ProcessingBase, ICustomStart, ICustomUpdate, IReceive<DamageSignal>, ICustomDisable
+public class TakeDamageProc : ProcessingBase, ICustomStart, ICustomUpdate, IReceive<DamageSignal>, IReceive<PreparateDamageSignal>, ICustomDisable
 {
     Group DamageGiverGroup = Group.Create(new ComponentsList<AttackCmp>());
+    //Group PhisicsGroup
 
     public void OnStart()
     {
         SignalManager<DamageSignal>.AddReceiver(this);
+        SignalManager<PreparateDamageSignal>.AddReceiver(this);
     }
 
-    public void CustomUpdate()
-    {
-        /*foreach (int damageGiver in DamageGiverGroup)
-        {
-            DamageGiverCmp damageGiverCmp = Storage.GetComponent<DamageGiverCmp>(damageGiver);
+    public void CustomUpdate() { }
 
-            damageGiverCmp.time_after_last_attack += Time.deltaTime;
-        }*/
-    }
 
     public void SignalHandler(DamageSignal arg)
     {
-        //arg.attackCmp.time_after_last_attack = 0;
-        Debug.Log("объекту " + arg.attackTarget.name + " нанесен урон " + arg.attackParameters.damage + " от " 
+        EntityBase target = arg.attackTarget;
+        AttackCmp attackCmp = arg.attaker.GetCmp<AttackCmp>();
+        Attack attack = arg.attackParameters;
+
+
+        if (target.TryGetCmp(out HealthCmp healthCmp))
+        {
+            healthCmp.health -= arg.attackParameters.damage;
+        }
+
+        if (target.TryGetCmp(out MoverCmp moverCmp) && target.TryGetCmp(out PhysicsCmp physicsCmp))
+        {
+            physicsCmp.Rigidbody.AddForce(((Vector2)(target.transform.position - attackCmp.transform.position)) * attack.pushForce);
+        }
+
+
+
+
+
+
+        Debug.Log("объекту " + arg.attackTarget.name + " нанесен урон " + arg.attackParameters.damage + " от "
             + arg.attaker.gameObject.name + " (" + arg.attaker.entity + ")");
-        Storage.GetComponent<HealthCmp>(arg.attackTarget.entity).health -= arg.attackParameters.damage;
     }
 
+    public void SignalHandler(PreparateDamageSignal arg)
+    {
+        RaycastHit[] castInfos = CreateCorrectRaycastHits(arg.raycastHits);
+        int sender = arg.damageSignal.attaker.entity;
+        Attack attack = arg.damageSignal.attackParameters;
+        AttackCmp attackCmp = arg.damageSignal.attackTarget.GetCmp<AttackCmp>();
+
+        for (int i = 0; i < castInfos.Length; i++)
+        {
+            if (castInfos[i].collider.attachedRigidbody.TryGetComponent(out EntityBase target))
+            {
+                if (target.entity != sender)
+                {
+                    SignalManager<DamageSignal>.SendSignal(new DamageSignal(attack, target, attackCmp.entityBase));
+                }
+            }
+        }
+    }
 
     public void OnCustomDisable()
     {
         SignalManager<DamageSignal>.RemoveReceiver(this);
+        SignalManager<PreparateDamageSignal>.RemoveReceiver(this);
+    }
+
+
+    static RaycastHit[] CreateCorrectRaycastHits(RaycastHit[] array)
+    {
+        Dictionary<Rigidbody, RaycastHit> Some = new Dictionary<Rigidbody, RaycastHit>();
+        Rigidbody rigidbody;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            rigidbody = array[i].collider.attachedRigidbody;
+            if (rigidbody != null)
+                if (!Some.ContainsKey(rigidbody))
+                    Some.Add(rigidbody, array[i]);
+        }
+
+        if (Some.Keys.Count == array.Length)
+            return array;
+
+        array = new RaycastHit[Some.Keys.Count];
+
+        int index = 0;
+        foreach (RaycastHit hit in Some.Values)
+            array[index++] = hit;
+
+        return array;
     }
 }
